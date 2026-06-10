3472b730f6449498e270b93ca8c9e4f5c50a15b8
drmagregro <114160314+drmagregro@users.noreply.github.com>
2026-06-10 16:41:03 +0200

    feature: add backend and try it with a photo of a random ticket

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import sys
 import base64
 import json
 from dotenv import load_dotenv
@@ -6,12 +7,42 @@ import os
 
 class ExpenseAgent:
     def __init__(self):
-        load_dotenv (".env")
-        api_key = os.getenv("GROQ_API_KEY")
-        client = Groq(api_key)
+        load_dotenv()
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(BASE_DIR, "context.txt"), "r", encoding="utf-8") as f:
+            self.context = f.read()
+        with open(os.path.join(BASE_DIR, "prompt.txt"), "r", encoding="utf-8") as f:
+            self.prompt = f.read()
         
 
-        pass
-
     def extract_from_bytes(self, image_bytes, media_type):
-        pass
+        image_b64 = base64.b64encode(image_bytes).decode("utf-8")
+        response = self.client.chat.completions.create(
+            model="meta-llama/llama-4-scout-17b-16e-instruct",
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": self.context},
+                {"role": "user", "content": [
+                    {"type": "text", "text": self.prompt},
+                    {"type": "image_url", "image_url": {
+                        "url": f"data:{media_type};base64,{image_b64}"
+                    }}
+                ]}
+            ]
+        )
+        EXPECTED_FIELDS = ["type_document", "fournisseur", "date", "montant_ttc", "tva", "devise", "description", "confiance"]
+        result = json.loads(response.choices[0].message.content)
+        return {field: result.get(field, None) for field in EXPECTED_FIELDS}
+    
+
+if __name__ == "__main__":
+    
+    image_path = sys.argv[1]  # on passe le chemin de l'image en argument
+    
+    with open(image_path, "rb") as f:
+        image_bytes = f.read()
+    
+    agent = ExpenseAgent()
+    result = agent.extract_from_bytes(image_bytes, "photo-ticket.jpg")
+    print(json.dumps(result, indent=2, ensure_ascii=False))
